0748f72a75e66cf6c8e741993352ea510494f17a
hokagn <184084617+hokagn@users.noreply.github.com>
2025-02-12 21:06:31 +0900

    Result.tsx fix

--- src/Result.tsx
+++ src/Result.tsx
@@ -1,4 +1,4 @@
-import { useLayoutEffect, useRef, useState } from "react";
+import { useLayoutEffect, useState } from "react";
 import styled from "styled-components";
 import { CBTI } from "./Test";
 import { Link } from "react-router-dom";
